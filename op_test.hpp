9a2e46deba78592d189c44399349ac28505a63cd
michael07w <michaelwessels207@gmail.com>
2021-11-02 15:34:04 -0700

    Add Op implementation

--- op_test.hpp
+++ op_test.hpp
@@ -7,7 +7,22 @@
 
 TEST(OpTest, OpEvaluateNonZero) {
     Op* test = new Op(8);
-    EXPECT_EQ(test->evaluate(), 8);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 8);
+}
+
+TEST(OpTest, OpEvaluateNegative) {
+    Op* test = new Op(-2);
+    EXPECT_DOUBLE_EQ(test->evaluate(), -2);
+}
+
+TEST(OpTest, OpEvaluateZero) {
+    Op* test = new Op(0);
+    EXPECT_DOUBLE_EQ(test->evaluate(), 0);
+}
+
+TEST(OpTest, OpStringify) {
+    Op* test = new Op(7);
+    EXPECT_EQ(test->stringify(), "7");
 }
 
 #endif //__OP_TEST_HPP__
